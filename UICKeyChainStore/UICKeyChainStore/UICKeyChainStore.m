02c25728ae5956c92fbd686e45167c9ea843ba6f
Nate Minshew <nminshew@nminshew-ml3.asolutions.com>
2012-12-12 11:11:55 -0600

    Converted to framework project.

---
+++ UICKeyChainStore/UICKeyChainStore/UICKeyChainStore.m
@@ -0,0 +1,13 @@
+//
+//  UICKeyChainStore.m
+//  UICKeyChainStore
+//
+//  Created by Nate Minshew on 12/12/12.
+//  Copyright (c) 2012 Asynchrony. All rights reserved.
+//
+
+#import "UICKeyChainStore.h"
+
+@implementation UICKeyChainStore
+
+@end
